f11c70e937dd48102b2438655cb00d999e5ff6f3
Pano Papadatos <pano.papadatos@publicrelay.com>
2019-01-21 12:47:41 -0500

    first commit

---
+++ Unfortunate Tale/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public string playerName;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        float verticalMove = Input.GetAxisRaw("Vertical");
+        bool isRight = horizontalMove > 0.5f;
+        bool isLeft = horizontalMove < -0.5f;
+        bool isUp = verticalMove > 0.5f;
+        bool isDown = verticalMove < -0.5f;
+        bool isHorizontalMove = isRight || isLeft;
+        bool isVerticalMove = isUp || isDown;
+        // bool isMove = isHorizontalMove || isVerticalMove;
+        float verticalTranslate = verticalMove * moveSpeed * Time.deltaTime;
+        float horizontalTranslate = horizontalMove * moveSpeed * Time.deltaTime;
+        if (isHorizontalMove)
+        {
+            transform.Translate(new Vector3(horizontalMove, 0f, 0f));
+        }
+        if (isVerticalMove)
+        {
+            transform.Translate(new Vector3(0f, verticalMove, 0f));
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
