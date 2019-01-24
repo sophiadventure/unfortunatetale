5ac760f0de3eebbb04d484ebfc2444d61de2d616
Pano Papadatos <pano.papadatos@publicrelay.com>
2019-01-24 10:26:10 -0500

    Adding assets/ moving assets around

---
+++ Unfortunate Tale/Assets/Scripts/AreaExit.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class AreaExit : MonoBehaviour
+{
+
+    public string areaToLoad;
+    public string otherTag;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        //otherTag = other.tag;        
+        //if(other.tag == "Player")
+        //{
+        //    SceneManager.LoadScene(areaToLoad);
+        //}
+    }
+}
